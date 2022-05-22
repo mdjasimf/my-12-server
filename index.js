5b4d20fe31e407fdd2f961c012280a92a2bd59ae
mdjasimf <mdjasimf@gmail.com>
2022-05-22 10:20:41 +0600

    test

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nipoy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('manufacture').collection('tools');
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+
+
+
+
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello 12 assignment!')
+})
+
+app.listen(port, () => {
+    console.log(`12 assignment listening on port ${port}`)
+})
